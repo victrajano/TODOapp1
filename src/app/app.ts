500d8462497635d37d796ce52e69622bc4bbc505
unisanta <unisanta@santacecilia.br>
2026-02-25 19:47:19 -0300

    Commit inicial

---
+++ src/app/app.ts
@@ -0,0 +1,31 @@
+import { Component, signal } from '@angular/core';
+import { Tarefa } from "./tarefa";
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  standalone: false,
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('TODOapp');
+  arrayDeTarefas: Tarefa[] = [];
+ constructor()
+  {
+    this.READ_tarefas();
+}
+CREATE_tarefa(descricaoNovaTarefa: string) {
+  var novaTarefa = new Tarefa(descricaoNovaTarefa, false);
+  this.arrayDeTarefas.unshift(novaTarefa);
+  }
+ 
+ READ_tarefas() {
+  this.arrayDeTarefas = [
+  new Tarefa("Estudar Frameworks WEB", false),
+  new Tarefa("Comer Pizza", false),
+  new Tarefa("Ajudar meus pais", false)
+ ]; 
+}
+}
+
